3c016139c0871da7242b3ee34b72653cc6999d1f
maihoangviet <viet.mai.1@fecredit.com.vn>
2025-06-07 03:56:00 +0700

    update scheduler

--- jobs/schedule_5m.py
+++ jobs/schedule_5m.py
@@ -1,30 +1,27 @@
-from datetime import datetime
 from pytz import timezone
 import time
+from pathlib import Path
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from apscheduler.triggers.cron import CronTrigger
 
-from download_data import DownloadStockFactory
-from helper.update_git import GitPusher
+from utils.shells import run_sh
 
 
-src_VN30F = DownloadStockFactory(symbol="VN30F", from_date_yyyymmdd="20250601", to_date_yyyymmdd="20250601", dry_run=True)
-src_VN30 = DownloadStockFactory(symbol="VN30", from_date_yyyymmdd="20250601", to_date_yyyymmdd="20250601", dry_run=True)
-git_helper = GitPusher(repo_path="..")
-
-
-def get_data_today():
-    git_helper.pull()
-    run_dttm = datetime.now().strftime("%Y%m%d")
-    src_VN30F.download(from_date_yyyymmdd=run_dttm, to_date_yyyymmdd=run_dttm)
-    src_VN30.download(from_date_yyyymmdd=run_dttm, to_date_yyyymmdd=run_dttm)
-    git_helper.push()
+def run_task():
+    try:
+        base_dir = Path(__file__).resolve().parent
+        script = base_dir / "tasks" / "update_stock_price_5m.py"
+        run_sh(command=f"python3.10 {script.as_posix()}")
+    except Exception as e:
+        print("run_task failed: %s", e)
 
 
 if __name__ == "__main__":
+    vn_tz = timezone("Asia/Ho_Chi_Minh")
+
     job_stores = {
         'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
     }
@@ -36,33 +33,33 @@ if __name__ == "__main__":
         'coalesce': False,
         'max_instances': 4
     }
-    scheduler = BackgroundScheduler(jobstores=job_stores, executors=executors, job_defaults=job_defaults, timezone=timezone("Asia/Ho_Chi_Minh"))
+    scheduler = BackgroundScheduler(jobstores=job_stores, executors=executors, job_defaults=job_defaults, timezone=vn_tz)
 
     # At 08:45, 08:50, 08:55 on Mon–Fri
     trigger_early = CronTrigger(
         day_of_week="mon-fri",
         hour="8",
         minute="45,50,55",
-        timezone="Asia/Ho_Chi_Minh"
+        timezone=vn_tz,
     )
     # Every 5 minutes between 09:00 and 13:55 on Mon–Fri
     trigger_midday = CronTrigger(
         day_of_week="mon-fri",
         hour="9-13",
         minute="*/5",
-        timezone="Asia/Ho_Chi_Minh"
+        timezone=vn_tz
     )
     # At 14:00, 14:05, …, 14:45 on Mon–Fri
     trigger_late = CronTrigger(
         day_of_week="mon-fri",
         hour="14",
         minute="0-45/5",
-        timezone="Asia/Ho_Chi_Minh"
+        timezone=vn_tz
     )
 
-    scheduler.add_job(get_data_today, trigger_early, id="job_08_early")
-    scheduler.add_job(get_data_today, trigger_midday, id="job_09_13_mid")
-    scheduler.add_job(get_data_today, trigger_late, id="job_14_late")
+    scheduler.add_job(run_task, trigger_early, id="job_08_early")
+    scheduler.add_job(run_task, trigger_midday, id="job_09_13_mid")
+    scheduler.add_job(run_task, trigger_late, id="job_14_late")
     scheduler.start()
 
     try:
---
+++ jobs/tasks/update_stock_price_5m.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from download_data import DownloadStockFactory
+from helper.update_git import GitPusher
+
+
+src_VN30F = DownloadStockFactory(symbol="VN30F", from_date_yyyymmdd="20250601", to_date_yyyymmdd="20250601", dry_run=True)
+src_VN30 = DownloadStockFactory(symbol="VN30", from_date_yyyymmdd="20250601", to_date_yyyymmdd="20250601", dry_run=True)
+git_helper = GitPusher(repo_path="..")
+
+
+def get_data_today():
+    git_helper.pull()
+    run_dttm = datetime.now().strftime("%Y%m%d")
+    src_VN30F.download(from_date_yyyymmdd=run_dttm, to_date_yyyymmdd=run_dttm)
+    src_VN30.download(from_date_yyyymmdd=run_dttm, to_date_yyyymmdd=run_dttm)
+    git_helper.push()
