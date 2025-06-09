46e41e9d0f733834e9da01c6519615bbb6ab5b10
maihoangviet <viet.mai.1@fecredit.com.vn>
2025-06-09 21:56:03 +0700

    add start jobs

--- start_jobs.sh
+++ start_jobs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-BASE_PATH=/apps/vnstockdata/
+BASE_PATH=/apps/vnstockdata
+export PYTHONPATH=$BASE_PATH
 source $BASE_PATH/.venv/bin/activate
 nohup python3 $BASE_PATH/jobs/schedule_5m.py >> $BASE_PATH/logs/scheduler.log 2>&1 &
