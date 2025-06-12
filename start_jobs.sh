bcd3c1a0d6b6521cd4fe2da475b08fd2dc1c044b
maihoangviet <viet.mai.1@fecredit.com.vn>
2025-06-13 02:19:20 +0700

    update job

--- start_jobs.sh
+++ start_jobs.sh
@@ -19,5 +19,8 @@ export PYTHONPATH=$BASE_PATH
 source $BASE_PATH/.venv/bin/activate
 echo "$(date +'%Y-%m-%d %H:%M:%S') ----------------------- START schedule_5m.py ----------------------------" >> "$LOG_FILE"
 
+pkill -f '/schedule_5m\.py$' 2>/dev/null || true
+sleep 3
+
 # 4) Launch
 nohup python3 jobs/schedule_5m.py >> "$LOG_FILE" 2>&1 &
